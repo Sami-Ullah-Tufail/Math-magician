e09a2d5e6e2b2aa09e907a2ce4a0c3714171ccae
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2023-09-23 23:05:41 +0500

    conflicts resolution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Calculator from './components/Calculator';
 import './components/calculator.css';
+import ShowQuotes from './components/ShowQuotes';
+import './components/quotes.css';
 
 export default function App() {
-  return <Calculator />;
+  return (
+    <div>
+      <Calculator />
+      {' '}
+      <ShowQuotes />
+    </div>
+  );
 }
